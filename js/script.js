de5f3a1e86bcac1e2088240c02be507623c12c0e
webdev <tokkerbaz@gmail.com>
2021-06-21 15:43:21 +0600

    добавлен слайдер

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
